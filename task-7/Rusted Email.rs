6630bc85e0c79f9d8fad669ef896e677e6f8edb2
Govind <52847415+govgoel@users.noreply.github.com>
2019-08-11 18:03:10 +0530

    Update Rusted Email.rs

--- task-7/Rusted Email.rs
+++ task-7/Rusted Email.rs
@@ -1,1 +1,8 @@
+use regex::Regex;
 
+fn main() 
+{
+let re : Regex = Regex::new( re r"^([a-zA-z0-9_\-\.]+)@([a-zA-z0-9_\-\.])\.([a-zA-Z]{2,5})$").unwrap();
+assert!(re.is_match(text: "example@gmail.com"));
+println!("valid email");
+}
